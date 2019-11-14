1fd4c9d8e716fecceb8268d5fd80b65c15a6b472
WhyWeStillHere <runb2000@gmail.com>
2019-11-14 07:08:28 +0000

    Update bash scripts

--- bash_scripts/run_nodes.sh
+++ bash_scripts/run_nodes.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-if [ -n "$1" ]
-then
-	cd "$1"
-else
-	cd /opt/pgsql/
-fi
 
-cd bin/
+pkill -SIGABRT postgres
+
 
 if [ ! -d ~/LocalDB ]; then
 	echo "No Database!"
-else 
-	echo "Start of Primary node:"
-	./pg_ctl -D ~/LocalDB/Logic_node start
-
-	for node_number in {1..3}
-	do
-		echo "Start storage node_storage node_"$node_number" on port: 543$((2 + $node_number))"
-		./pg_ctl -D ~/LocalDB/Storage_node_$node_number/ start
-	done
-
+	exit 1
 fi
+
+cd ../aurora_logic_node/pgsql1/bin
+
+echo "Start Primary node:"
+./pg_ctl -D ~/LocalDB/Logic_node start
+
+
+cd ../../../aurora_storage_node/pgsql1/bin
+
+for node_number in {1..1}
+do
+	echo "Start storage node_storage node_"$node_number""
+	./pg_ctl -D ~/LocalDB/Storage_node_$node_number/ start
+done
+
 
 
 
--- bash_scripts/shutdown_nodes.sh
+++ bash_scripts/shutdown_nodes.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-if [ -n "$1" ]
-then
-	cd "$1"
-else
-	cd /opt/pgsql/
+if [ ! -d ~/LocalDB ]; then
+	echo "No Database!"
+	exit 1
 fi
 
-cd bin/
 
-for node_number in {1..3}
+cd ../aurora_logic_node/pgsql1/bin
+
+echo "Stop Primary node:"
+./pg_ctl -D ~/LocalDB/Logic_node stop
+
+cd ../../../aurora_storage_node/pgsql1/bin
+
+for node_number in {1..1}
 do
-	echo "Stoping storage node_"$node_number" on port: 543$((2 + $node_number))"
+	echo "Stop storage node_storage node_"$node_number""
 	./pg_ctl -D ~/LocalDB/Storage_node_$node_number/ stop
 done
 
-echo "Stopping of Primary node:"
-./pg_ctl -D ~/LocalDB/Logic_node stop
+
+
